65296c29903e32bbce49897a333da625dc1cc1db
Dodesimo <devammondal06@gmail.com>
2020-05-29 22:24:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/contactintentapp/CreateContact.java
@@ -0,0 +1,13 @@
+package com.example.android.contactintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_contact);
+    }
+}
