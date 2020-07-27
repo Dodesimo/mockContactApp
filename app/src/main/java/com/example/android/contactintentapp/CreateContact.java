7d670c18870780dba436fc9a43f2bfa4098e3c08
Dodesimo <devammondal06@gmail.com>
2020-07-26 22:24:38 -0400

    Project was committed.

--- app/src/main/java/com/example/android/contactintentapp/CreateContact.java
+++ app/src/main/java/com/example/android/contactintentapp/CreateContact.java
@@ -1,5 +1,14 @@
 package com.example.android.contactintentapp;
 
+//Bunch of imports.
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
